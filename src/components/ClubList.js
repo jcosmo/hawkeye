4d614df64bd6a3731868594bccda28bab5f8cad1
James Walker <github@jzwalker.id.au>
2018-02-27 07:43:25 +1100

    reformat

--- src/components/ClubList.js
+++ src/components/ClubList.js
@@ -6,16 +6,14 @@ import {inject, observer} from 'mobx-react';
 @observer
 class ClubList extends Component {
   render() {
-    if ( this.props.clubStore.loading )
-    {
+    if (this.props.clubStore.loading) {
       return <div>Loading...</div>;
     }
     const clubs = this.props.clubStore.clubs;
-    if ( 0 === clubs.length )
-    {
+    if (0 === clubs.length) {
       return <div>No clubs</div>;
     }
-    const content = clubs.slice(0).sort( (a,b) => a.name.localeCompare(b.name) ).map(club => <li key={club.id}>
+    const content = clubs.slice(0).sort((a, b) => a.name.localeCompare(b.name)).map(club => <li key={club.id}>
       <Link to={this.props.match.url + "/" + club.id}>{club.name}</Link>
     </li>);
 
